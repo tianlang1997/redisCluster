39e94df628405273295a7b6e3474c5d4b777264c
tianlang1997 <tianlang1997@126.com>
2019-05-31 09:52:23 +0800

    Update findRedisIP.sh

--- findRedisIP.sh
+++ findRedisIP.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "">ipList
+
+echo "">clusterIP
 for ((i=0; i<6; i++))
 do
-docker inspect redis700$i | grep IPAddress |grep -E "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" >>ipList
+docker inspect redis700$i | grep IPAddress | egrep -o "([0-9]{1,3}.){3}[0-9]{1,3}" >> clusterIP
 done
+"redis-cli --cluster create 172.17.0.3:6379 172.17.0.4:6379 172.17.0.5:6379 172.17.0.6:6379 172.17.0.7:6379 172.17.0.8:6379 --cluster-replicas 1" >> clusterIP
+echo "redis-cli --cluster create 172.17.0.3:6379 172.17.0.4:6379 172.17.0.5:6379 172.17.0.6:6379 172.17.0.7:6379 172.17.0.8:6379 --cluster-replicas 1"
